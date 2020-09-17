f2b80b1d6b8a1a0f02218c0aa045c55f0c5bf710
magical-builder-5600 <gabbinguyen@gmail.com>
2020-09-17 00:29:06 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,3 +6,8 @@ function updatePlaylist(playlist, name, title) {
   playlist[name] = title;
   return playlist;
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistname;
+  return playlist;
+}
